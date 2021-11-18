d72613b1dfd6886dce32b12b9802e5e3512f3789
qyylylx <2669231408@qq.com>
2021-11-18 23:53:02 +0800

    完成app1.0的布局

--- app/src/main/java/com/qyylylx/carcontroller/MainActivity.kt
+++ app/src/main/java/com/qyylylx/carcontroller/MainActivity.kt
@@ -2,10 +2,17 @@ package com.qyylylx.carcontroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.qyylylx.carcontroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.Forward.setOnClickListener {  }
+        binding.Backward.setOnClickListener {  }
+        binding.Left.setOnClickListener {  }
+        binding.Right.setOnClickListener {  }
     }
 }
